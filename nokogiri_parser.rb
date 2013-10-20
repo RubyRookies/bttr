d26303d2fd5aabafcaded283f0aa69a8872318a1
dklounge <daviddarden11@gmail.com>
2013-10-20 13:59:11 -0700

    updated parser

--- nokogiri_parser.rb
+++ nokogiri_parser.rb
@@ -1,5 +1,14 @@
 require 'rubygems'
 require 'nokogiri'
+require 'open-uri'
 
-page = Nokogiri::HTML(open('index.html'))
-puts page
+PAGE_URL = "http://ruby.bastardsbook.com/files/hello-webpage.html"
+page = Nokogiri::HTML(open(PAGE_URL))
+links = page.css('a')
+divs = page.css('div')
+# links.each { |t| puts t['href'] }
+puts divs.css('#references p a')
+
+# require 'restclient'
+# page = RestClient.get("http://en.wikipedia.org/")
+# puts page
